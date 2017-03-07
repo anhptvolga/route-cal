7fb4e26c189743e62ddd1a85c836ca1186404f9d
anton <anton@localhost.com>
2017-03-07 11:03:36 +0300

    first program\ ui.

--- route-calc/wmain.cpp
+++ route-calc/wmain.cpp
@@ -4,9 +4,62 @@ WMain::WMain(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	// connect signals and slots
+	connect(ui.bt_add_project, SIGNAL(clicked()), this, SLOT(on_add_project_clicked()));
+	connect(ui.bt_delete_project, SIGNAL(clicked()), this, SLOT(on_del_project_clicked()));
+	connect(ui.bt_rename_project, SIGNAL(clicked()), this, SLOT(on_ren_project_clicked()));
+	
+	connect(ui.bt_add_route, SIGNAL(clicked()), this, SLOT(on_add_route_clicked()));
+	connect(ui.bt_delete_route, SIGNAL(clicked()), this, SLOT(on_del_route_clicked()));
+	connect(ui.bt_rename_route, SIGNAL(clicked()), this, SLOT(on_ren_route_clicked()));
+	
+	connect(ui.bt_calculate, SIGNAL(clicked()), this, SLOT(on_calc_clicked()));
+
+	connect(ui.bt_save_text, SIGNAL(clicked()), this, SLOT(on_save_file_clicked()));
+
 }
 
 WMain::~WMain()
+{
+
+}
+
+void WMain::on_add_project_clicked() 
+{
+
+}
+
+void WMain::on_del_project_clicked()
+{
+
+}
+
+void WMain::on_ren_project_clicked()
+{
+
+}
+
+void WMain::on_add_route_clicked()
+{
+
+}
+
+void WMain::on_del_route_clicked()
+{
+
+}
+
+void WMain::on_ren_route_clicked()
+{
+
+}
+
+void WMain::on_calc_clicked()
+{
+
+}
+
+void WMain::on_save_file_clicked()
 {
 
 }
--- route-calc/wmain.h
+++ route-calc/wmain.h
@@ -12,8 +12,20 @@ public:
 	WMain(QWidget *parent = 0);
 	~WMain();
 
+public slots:
+	void on_add_project_clicked();
+	void on_del_project_clicked();
+	void on_ren_project_clicked();
+
+	void on_add_route_clicked();
+	void on_del_route_clicked();
+	void on_ren_route_clicked();
+
+	void on_calc_clicked();
+	void on_save_file_clicked();
+
 private:
-	Ui::WMainClass ui;
+	Ui::MainWindow ui;
 };
 
 #endif // WMAIN_H
