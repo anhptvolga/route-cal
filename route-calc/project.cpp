6c9c308b8b5249e5350939bcb4b167ac374d8a73
anton <anton@localhost.com>
2017-03-07 16:32:59 +0300

    add first version of project class

---
+++ route-calc/project.cpp
@@ -0,0 +1,28 @@
+#include "project.h"
+
+Project::Project()
+{
+
+}
+
+Project::Project(QString name)
+{
+	this->name = name;
+}
+
+Project::~Project()
+{
+
+}
+
+Route Project::get_route(int index)
+{
+	if (0 <= index && index < this->routes.count())
+		return this->routes[index];
+	return nullptr;
+}
+
+void Project::add_route(QString name)
+{
+	this->routes.append(Route(name));
+}
---
+++ route-calc/project.h
@@ -0,0 +1,27 @@
+#ifndef PROJECT_H
+#define PROJECT_H
+
+#include <QObject>
+#include <QVector>
+#include "route.h"
+
+class Project
+{
+
+public:
+	Project();
+	Project(QString name);
+	~Project();
+
+	void add_route(QString name);
+	void rename_route(QString newnName);
+	void delete_route(QString name);
+
+	Route get_route(int index);
+
+private:
+	QString name;
+	QVector<Route> routes;
+};
+
+#endif // PROJECT_H
