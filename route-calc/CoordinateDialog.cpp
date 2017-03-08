90752af51eeafe31cae814debaa2a68a2ac1f0f5
anton <anton@localhost.com>
2017-03-08 23:19:26 +0300

    add loading old coordinate to table and fix saving

--- route-calc/CoordinateDialog.cpp
+++ route-calc/CoordinateDialog.cpp
@@ -10,6 +10,30 @@ CoordinateDialog::CoordinateDialog(QWidget *parent, Route *prt)
 	connect(ui.bt_add, SIGNAL(clicked()), this, SLOT(on_add_clicked()));
 	connect(ui.bt_finish, SIGNAL(clicked()), this, SLOT(on_finish_clicked()));
 
+	// show old coordiate
+	if (proute != nullptr) 
+	{
+		QVector<Point> points = proute->get_points();
+		this->ui.tableWidget->setRowCount(points.count());
+		for (int i = 0; i < points.count(); ++i)
+		{
+			QTableWidgetItem *it = new QTableWidgetItem();
+			it->setTextAlignment(Qt::AlignHCenter|Qt::AlignVCenter|Qt::AlignCenter);
+			it->setText(QString::number(points[i].get_x()));
+			this->ui.tableWidget->setItem(i, 0, it);
+
+			it = new QTableWidgetItem();
+			it->setTextAlignment(Qt::AlignHCenter|Qt::AlignVCenter|Qt::AlignCenter);
+			it->setText(QString::number(points[i].get_y()));
+			this->ui.tableWidget->setItem(i, 1, it);
+
+			it = new QTableWidgetItem();
+			it->setTextAlignment(Qt::AlignHCenter|Qt::AlignVCenter|Qt::AlignCenter);
+			it->setText(QString::number(points[i].get_z()));
+			this->ui.tableWidget->setItem(i, 2, it);
+		}
+		this->on_add_clicked();
+	}
 }
 
 CoordinateDialog::~CoordinateDialog()
@@ -39,14 +63,14 @@ void CoordinateDialog::on_finish_clicked()
 {
 	QVector<Point> pts;
 	int rows = this->ui.tableWidget->rowCount();
+	QString x, y, z;
 	for (int i = 0; i < rows; ++i) 
 	{
-		qDebug() <<  this->ui.tableWidget->itemAt(i, 0)->text();
-		pts.append(Point(
-			this->ui.tableWidget->item(i, 0)->text().toDouble(),
-			this->ui.tableWidget->item(i, 1)->text().toDouble(),
-			this->ui.tableWidget->item(i, 2)->text().toDouble()
-			));
+		x = this->ui.tableWidget->item(i, 0)->text();
+		y = this->ui.tableWidget->item(i, 1)->text();
+		z = this->ui.tableWidget->item(i, 2)->text();
+		if (!(x.isEmpty() || y.isEmpty() || z.isEmpty()))
+			pts.append(Point(x.toDouble(), y.toDouble(), z.toDouble()));
 	}
 	this->proute->update_points(pts);
 	this->done(0);
