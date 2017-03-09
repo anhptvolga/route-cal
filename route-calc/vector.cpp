5105baa62c2ac48bbadd2080f42660b2b1daea5e
anton <anton@localhost.com>
2017-03-09 08:55:37 +0300

    written getters

--- route-calc/vector.cpp
+++ route-calc/vector.cpp
@@ -1,6 +1,5 @@
 #include "vector.h"
 
-
 Vector::Vector()
 	: x(0), y(0), z(0)
 {
@@ -17,5 +16,41 @@ Vector::Vector(Point starting, Point ending)
 	this->x = ending.get_x() - starting.get_x();
 	this->y = ending.get_y() - starting.get_y();
 	this->z = ending.get_z() - starting.get_z();
+}
+
+QString Vector::to_string()
+{
+	QString res("(");
+	res = res + QString::number(x) + "; " +
+		QString::number(y) + "; " +
+		QString::number(z) + ")";
+	return res;
+}
+
+double Vector::get_x()
+{
+	return this->x;
+}
+
+double Vector::get_y()
+{
+	return this->y;
+}
+
+double Vector::get_z()
+{
+	return this->z;
+}
+
+
+bool Vector::is_parallel(Vector other)
+{
+	double cosalpha = (this->x * other.x + this->y * other.y + this->z * other.z) / (this->get_len() * other.get_len());
+	return abs(1 - abs(cosalpha)) < 0.0000001;
+}
+
+double Vector::get_len()
+{
+	return sqrt(x*x + y*y + z*z);
 }
 
--- route-calc/vector.h
+++ route-calc/vector.h
@@ -1,6 +1,12 @@
-#pragma once
+#ifndef __VECTOR_H__
+#define __VECTOR_H__
 
+#include <cmath>
+#include <QString>
+#include "Setting.h"
 #include "point.h"
+
+using namespace std;
 
 class Vector
 {
@@ -9,8 +15,20 @@ public:
 	Vector(double _x, double _y, double _z);
 	Vector(Point starting, Point ending);
 
+	QString to_string();
+
+	bool is_parallel(Vector other);
+
+	double get_x();
+	double get_y();
+	double get_z();
+
+	double get_len();
+
 private:
 	double x;
 	double y;
 	double z;
 };
+
+#endif
